0d12108ea66c8303585c460e3b719ce6124622fa
TiavinaMichael <ralainirinatiavina@gmail.com>
2020-11-19 10:41:11 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from '../src/Image';
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Image Component With Layout Fixed</h1>
+      <Image layout="fixed" height={300} />
+  </div>
+  );
+}
